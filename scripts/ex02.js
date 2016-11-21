4443f81014bd910df01d9c2c2b507fa0f00babc2
macloo <mmcadams@gmail.com>
2016-11-21 11:00:23 -0500

    updates

--- scripts/ex02.js
+++ scripts/ex02.js
@@ -2,7 +2,7 @@ $(document).ready(function(){  // do not delete
 // EXERCISE 2
 
 // this part is to resize the "viewer" div for mobile only
-// it works for images with a 4:3 aspect ratio
+// it works for images with a 4:3 aspect ratio - specifically 800 x 600
 var viewerWidth = $( '#viewer' ).width();
 if (viewerWidth < 800) {
 	$( '#viewer' ).height( viewerWidth * 0.75 );
@@ -19,9 +19,11 @@ $( window ).resize(function() {
 
 // this makes the clicked image fade out and hide
 // also, the faded image is moved with .append() to the end of the queue
+// to make the slideshow endless
 $('.fadeitem').on('click', function() {
 	$(this).fadeOut('slow', function() {
 		$('#viewer').append(this);
+		// make it visible again
 		$(this).fadeIn('fast');
 	});
 });
