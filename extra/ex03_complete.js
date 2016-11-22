64a3ae644bf9ea745150f8c3b1c178269c421190
macloo <mmcadams@gmail.com>
2016-11-22 09:27:58 -0500

    updates to ex03

--- extra/ex03_complete.js
+++ extra/ex03_complete.js
@@ -1,30 +1,31 @@
 $(document).ready(function(){  // do not delete
-// ----------------------------------------------------------------------------
 // EXERCISE 3
-// In the HTML file, first look at the code to see what has been hidden
-// with this jQuery.
-// Then figure out how to make the appropriate character appear when we
-// hover on his or her name in the main text.
-//
 // Hint: From Code School, do you remember this?
 // var amount = $(this).closest('.vacation').data('price');
-// ----------------------------------------------------------------------------
 
 
+// associate each character DIV with a new variable, named the same as
+// the ID of that DIV
 var rama = $('#rama');
 var sita = $('#sita');
 var hanuman = $('#hanuman');
 var ravana = $('#ravana');
 var lakshmana = $('#lakshmana');
+var person = "";
 
+// hide all the character DIVs
 $('.character').hide();
 
+
 // handles the data attributes, which are inside tags in the HTML
+// like this:  data-person="#rama"
+// each character's name is inside a span with class="name"
+// so when you hover on a span, jQuery grabs the data attribute for person
+// and toggles (shows) the associated DIV, using the variable defined above
 $('.name').hover(function() {
-	var person = $(this).closest('span').data('person');
+	person = $(this).closest('span').data('person');
 	$(person).toggle();
 });
-
 
 
 // this part is to resize the "viewer" div for mobile only
@@ -34,6 +35,18 @@ if ( winWidth < 800 ) {
 } else {
     $('.right').prepend("<p>Roll over a character's name at left to learn more.</p>");
 }
+$('.name').click(function() {
+	if ( winWidth < 800 ) {
+		person = $(this).closest('span').data('person');
+		$(person).show().prependTo( $(this).parent() );
+	}
+});
+$('.character').click(function() {
+	if ( winWidth < 800 ) {
+		$(person).hide();
+	}
+});
+
 
 // ----------------------------------------------------------------------------
 }); // do not delete
