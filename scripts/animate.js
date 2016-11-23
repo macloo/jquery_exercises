739953ff16b3d7834b04e6e1173c75fb3229fc1a
macloo <mmcadams@gmail.com>
2016-11-23 18:27:17 -0500

    update animate example

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,49 +1,50 @@
 $(document).ready(function() {
 // ---------------------------------------------------------------------------
-// This comes from Codecademy jQuery Part 4: 11/12
+// Inspired by Codecademy jQuery Part 4: 11/12
 // http://www.codecademy.com/tracks/jquery
-// It's a fine example of a switch statement, which is straight JavaScript. 
+// It's a fine example of a switch statement, which is straight JavaScript.
 // It saves us from writing lots of "if-else if-else" lines.
-// NOTE position styles in the CSS file.
+// NOTE: the position styles are in the CSS file.
 // ---------------------------------------------------------------------------
-// If you need to find key codes for other keys, including the numbers 
-// and letters, just look them up: 
-// http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+// If you need to find key values for other keys, try this fiddle:
+// https://jsfiddle.net/macloo/hzhx1hu2/
 // ---------------------------------------------------------------------------
 // EXERCISE
-// 1. Can you figure out how to make the rocket move diagonally? 
+// 1. Can you figure out how to make the rocket move diagonally?
 //    Hint: Diagonal is up and left, or down and right, etc.
-// 2. Can you create a button that returns the rocket to its original 
-//    position? 
-// 3. Can you write jQuery that hides the #wrapper the first time any 
+// 2. Can you create a button that returns the rocket to its original
+//    position?
+// 3. Can you write jQuery that hides the #wrapper the first time any
 //    key is pressed?
 // ---------------------------------------------------------------------------
 
-$(document).keydown(function(key) {
+// e stands for "event" - in this case, the event is a keypress
+// the arrow keys return: ArrowRight ArrowLeft ArrowUp ArrowDown
+// they are strings - note the quotation marks below
 
-	switch(parseInt(key.which,10)) { // see URL at bottom
-	
+$(document).on('keydown',function(e) {
+
+	switch(e.key) {
+
 		// left arrow pressed
-		case 37:
+		case "ArrowLeft":
 			$('#rocket').animate({left: "-=10px"}, 'fast');
 			break;
 		// up arrow pressed
-		case 38:
+		case "ArrowUp":
 			$('#rocket').animate({top: "-=10px"}, 'fast');
 			break;
 		// right arrow pressed
-		case 39:
+		case "ArrowRight":
 			$('#rocket').animate({left: "+=10px"}, 'fast');
 			break;
 		// down arrow pressed
-		case 40:
+		case "ArrowDown":
 			$('#rocket').animate({top: "+=10px"}, 'fast');
 			break;
 		}
 	});
 
-// ---------------------------------------------------------------------------
-// wondering about parseInt(key.which,10) ? See:
-// http://www.codecademy.com/forum_questions/50be3aafdf438796710005f3
+
 // ---------------------------------------------------------------------------
 }); // do not delete
